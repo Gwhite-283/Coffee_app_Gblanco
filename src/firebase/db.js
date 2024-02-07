c3951c4f52fe5883879a2664212cb4102c380b7c
Gonzalo White <gblanco.wl@gmail.com>
2024-02-07 18:06:56 -0300

    first commit

---
+++ src/firebase/db.js
@@ -0,0 +1,3 @@
+export const base_url = "https://ecommerce-blanco23-default-rtdb.firebaseio.com/"
+export const auth_base_url = "https://identitytoolkit.googleapis.com/v1/"
+export const api_key = "AIzaSyARRB9Xsonmn2gKZ-7URYfcAX4C-UcN--A"
