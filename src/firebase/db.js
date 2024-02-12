487c16821a3e037cab2c7160b125e2d8c4824fb8
Gonzalo White <gblanco.wl@gmail.com>
2024-02-12 01:18:49 -0300

    primer commit

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,3 +1,3 @@
-export const base_url = "https://ecommerce-blanco23-default-rtdb.firebaseio.com/"
+export const base_url = "https://app-coffee-01-default-rtdb.firebaseio.com/"
 export const auth_base_url = "https://identitytoolkit.googleapis.com/v1/"
-export const api_key = "AIzaSyARRB9Xsonmn2gKZ-7URYfcAX4C-UcN--A"
+export const api_key = "AIzaSyCIRklxwBex8zeHolLFnHvx4EXqQzcrVFE"
